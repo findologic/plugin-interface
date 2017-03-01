7f56c71674d181da8937e9db4fb1015fcaa0bf6c
BuildMonkey <buildmonkey@plentymarkets.eu>
2017-03-01 15:09:09 +0100

    Automated interface update 2017-03-01_15:09:09

--- Modules/Order/Pdf/Models/OrderPdfGeneration.php
+++ Modules/Order/Pdf/Models/OrderPdfGeneration.php
@@ -1,18 +1,19 @@
 <?php
-namespace Plenty\Modules\Category\Models;
+namespace Plenty\Modules\Order\Pdf\Models;
 
-use Plenty\Modules\System\Models\Webstore;
 
 /**
- * The Category Client
+ * OrderPdfGeneration Model
  */
-abstract class CategoryClient 
+abstract class OrderPdfGeneration 
 {
 
 	const CREATED_AT = 'createdAt';
 
 	const UPDATED_AT = 'updatedAt';
-	public		$plentyId;
+	public		$language;
+	public		$advice;
+	public		$sums;
 	
 	/**
 	 * Returns this model as an array.
