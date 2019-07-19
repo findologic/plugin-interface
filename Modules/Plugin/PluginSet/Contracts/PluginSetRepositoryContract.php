ee5ae1d086d704877ef24952e7c51ffac751d5c1
BuildMonkey <buildmonkey@plentymarkets.com>
2019-07-19 17:20:03 +0200

    Automated interface update 2019-07-19_17:20:03

--- Modules/Plugin/PluginSet/Contracts/PluginSetRepositoryContract.php
+++ Modules/Plugin/PluginSet/Contracts/PluginSetRepositoryContract.php
@@ -151,10 +151,4 @@ interface PluginSetRepositoryContract
 		string $pluginSetHash
 	):int;
 
-	/**
-	 * Get the PluginSetID of the currently running plugin.
-	 */
-	public function getCurrentPluginSetId(
-	):int;
-
 }
